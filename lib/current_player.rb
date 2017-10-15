8a96a17e089079868a1186a4f81a80834a15e89a
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-15 15:43:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
-def turn_count
-
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if (position == "X" || position == "O")
+      counter += 1
+    end
+  end
 end
 
 def current_player
